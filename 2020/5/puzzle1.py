287d55bb878223d81ce0b1e5a5f997088ec7093e
AutomatedTester <david.burns@theautomatedtester.co.uk>
2020-12-07 22:06:34 +0000

    Day 5 puzzle, has subtle flaw

---
+++ 2020/5/puzzle1.py
@@ -0,0 +1,42 @@
+import math
+
+
+def find_seat():
+    lines = []
+    with open("data.txt") as f:
+        lines = [line.strip() for line in f]
+
+    largest = 0
+    for line in lines:
+        row_data = line[:7]
+        seat_data = line[7:]
+        row_number = search(row_data, 0, 127)
+
+        seat_number = search(seat_data, 0, 7)
+
+        calc = row_number * 8 + seat_number
+        if calc > largest:
+            largest = calc
+
+    print(largest)
+
+
+def search(data, start, end):
+    result = 0
+    if data[0] in ["F", "B"]:
+        search_criteria = "F"
+    else:
+        search_criteria = "L"
+
+    for dat in data:
+        mid = math.floor((start + end)/2)
+        if dat == search_criteria:
+            end = mid
+        else:
+            start = mid
+        result = mid
+    return result
+
+
+if __name__ == "__main__":
+    find_seat()
